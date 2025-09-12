18dc0bd837e79d2daa871b4f354e73feb877b19b
LuisDFJ <luis.fuju@outlook.com>
2025-09-12 13:35:06 -0600

    Added theme toggle functionality.

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -13,7 +13,26 @@ if( toggle && navLinks && main && footer ){
     footer.addEventListener( 'click', () => {
         navLinks.classList.remove( 'active' );
     } );
-    
+}
+
+const toggle_theme_button = document.getElementById( 'toggle-theme-button' );
+const toggle_theme = document.getElementById( 'toggle-theme' );
+const body = document.getElementById( 'body' );
+if( toggle_theme_button && toggle_theme && body ){
+    toggle_theme_button.addEventListener( 'click', () => {
+        toggle_theme_button.classList.toggle( 'active' )
+        if( 'fa-moon' == toggle_theme.classList[1] ){
+            toggle_theme.classList.remove( 'fa-moon' )
+            toggle_theme.classList.add( 'fa-sun' )
+            body.classList.remove( 'dark' )
+            body.classList.add( 'light' )
+        } else {
+            toggle_theme.classList.remove( 'fa-sun' )
+            toggle_theme.classList.add( 'fa-moon' )
+            body.classList.remove( 'light' )
+            body.classList.add( 'dark' )
+        }
+    } );
 }
 
 
